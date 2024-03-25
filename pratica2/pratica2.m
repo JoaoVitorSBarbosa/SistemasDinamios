c0aea185cbd9f08ba3ace7925047ad79c1deeb8a
João Vitor Santos <jvsb2017@gmail.com>
2024-03-25 11:38:44 -0300

    pratica 2

---
+++ pratica2/pratica2.m
@@ -0,0 +1,84 @@
+%% Laboratório de Sistemas dinâmicos
+% Prática 02
+% Data: 25/03/2024
+% Autores: Ana Clara Gomes & João Vitor Barbosa
+
+
+%%Limpar Workspace
+clear all;
+close all;
+clc;
+
+%%
+% Script Prática 2
+
+%% Ex. 01
+
+syms a b c d;
+M = [a b; c d];
+
+disp('DET:');
+pretty(det(M));
+disp(' ');
+disp('INV:');
+pretty(inv(M));
+disp(' ');
+disp('TRACO:');
+pretty(trace(M));
+disp(' ');
+
+%% Ex. 02 (a)
+
+n = 0:10; 
+x_n = (-1).^n;
+
+subplot(3,1,1);
+stem(n,x_n);
+xlabel('n');
+ylabel('x[n]');
+
+subplot(3,1,2);
+stairs(n,x_n);
+xlabel('n');
+ylabel('x[n]');
+
+subplot(3,1,3);
+bar(n,x_n);
+xlabel('n');
+ylabel('x[n]');
+
+%% Ex. 02 (b)
+n = 0:10; 
+x_n = cos(((pi / 12).*n) + (pi/4));
+
+subplot(3,1,1);
+stem(n,x_n);
+xlabel('n');
+ylabel('x[n]');
+
+subplot(3,1,2);
+stairs(n,x_n);
+xlabel('n');
+ylabel('x[n]');
+
+subplot(3,1,3);
+bar(n,x_n);
+xlabel('n');
+ylabel('x[n]');
+
+
+%% Ex. 03
+
+y = zeros(1,11);
+y(1) = 10;
+y(2) = 22;
+
+for i = 3:10
+   y(i) = (2 * y(i -1)) + 2;  
+end
+
+disp(y);
+
+
+%% Ex. 04
+
