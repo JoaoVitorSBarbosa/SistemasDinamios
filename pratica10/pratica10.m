90f2c34122c31a7180d9ec4a097b699091174f2f
JoaoVitorSBarbosa <jvsb2017@gmail.com>
2024-08-25 23:20:00 -0300

    Finaliza praticas

--- pratica10/pratica10.m
+++ pratica10/pratica10.m
@@ -19,9 +19,9 @@ tempo = dados(:,1);
 entrada = dados(:,2);
 saida = dados(:,3);
 
-tm  = 150;
+tm  = 400;
 x1 = 48;
-x2 = 100;
+x2 = 260;
 y1 = 2;
 y2 = 6;
 
@@ -34,14 +34,14 @@ Ganho = mean(saida(end-50:end));
 
 saidaNormalizada = saida / Ganho;
 
-plot(tempo, saidaNormalizada, 'b', 'DisplayName', 'Dados experimentais'); 
+plot(tempo, saida, 'b', 'DisplayName', 'Dados experimentais'); 
 grid on;
 hold on;
 
 
 m1 = trapz(dados(:,1),1 - saidaNormalizada);
-disp(m1);
-disp(Mi);
+
+
 lambda = (tm - m1) * Mi;
 n = 1;
 tau1 = (n ^ (n/(1-n))) / Mi;
@@ -49,8 +49,22 @@ tau2 = (1 ^ (n/(1-n))) / Mi;
 taud = m1- tau1 - tau2;
 s = tf('s');
 Hs = (exp(-taud * s)) / (((tau1 * s) + 1) * ((tau1 * s) + 1));
-step(Hs,'r');
+step(Hs * 10,'r');
+disp(['m1: ' num2str(m1)]);
+disp(['Mi: ' num2str(Mi)]);
+disp(['lambda: ' num2str(lambda)]);
+disp(['n: ' num2str(n)]);
+disp(['tau1: ' num2str(tau1)]);
+disp(['tau2: ' num2str(tau2)]);
+disp(['taud: ' num2str(taud)]);
+grid on;
+hold off;
 
+% Para o modelo de segunda ordem, primeiramente deve-se entender os parâmetros de ganho e tm. 
+% O ganho é calculado pelo MatLab pelos 50 valores de estabilização ao final do sistema e tm é obtido visualmente,
+% traçando uma reta no ponto de inflexão e tomando o tempo em que o sistema se estabiliza no prolongamento dessa reta.
+% Para determinar os parâmetros usamos as equações presentes no slide
+% nota-se uma grande dificuldade devido a dependência da reta tangente
 %% Método de Mollenkamp
 dados = load('dadosordem2sobre.txt'); 
 t = dados(:,1);
@@ -86,62 +100,7 @@ hold on;
 % Neste método foi necessário obter três tempos: 
 % t1 = 43 quando a saída está em 15% do ganho, t2 = 83.5 quando a saída está em 45% e t3 = 143.5 quando a saída está em 75% . 
 % Esses tempos foram obtidos visualmente
-
-%% Exercicio 1 (b)
-% H(s) = e^(-Td*s)/(T1*s + 1)*(T2*s + 1)
-
-m1 = trapz(norm_u)*0.5 - trapz(norm_y)*0.5;
-tm = 140; % Encontrado de acordo com a inclinação da reta tangente
-Mi = 1/tm; % Inclinação da Reta Tangente
-lambda = (tm - m1)*Mi; % 0.2206
-n = 0.1; % Encontrado pelo gráfico, a partir do lambda
-T1 = n^(n/(1-n))/Mi; % Polo T1 do Sistema
-T2 = n^(1/(1-n))/Mi; % Polo T2 do Sistema
-Td = m1 - T1 - T2; % Teta (Tempo Morto)
-
-s = tf('s');
-Hs = exp(Td*s)/((T1*s + 1)*(T2*s + 1));
-hold on;
-step(tempo, Hs, 'r');
-grid on
-
-%% Método de Sundaresan
-dados = load('dadosordem2sobre.txt'); 
-t = dados(:,1);
-u = dados(:,2);
-y = dados(:,3);
-k =  mean(dados(end-50:end));
-kn = k/k;
-
-yn = y/k;
-teta_tau = trapz(t, u- yn);
-
-teta = 10;
-tm = 100;
-M1 = 1/(tm-teta);
-
-mi = teta_tau;
-lambda = (tm - mi)*M1;
-disp(lambda);
-n = 0.3;
-
-
-t1 = (n^(n/(1-n)))/M1;
-t2 = (n^(1/(1-n)))/M1;
-tauD = mi - t1 - t2;
-
-G1 = tf(kn, [(t1*t2) (t1 + t2) 1], 'InputDelay', tauD);
-plot(t,yn); 
-grid on
-hold on;
-step(G1, 'r');
-grid on
-hold on;
-
-% Para o modelo de segunda ordem, primeiramente deve-se entender os parâmetros de ganho e tm. 
-% O ganho é calculado pelo MatLab pelos 50 valores de estabilização ao final do sistema e tm é obtido visualmente,
-% traçando uma reta no ponto de inflexão e tomando o tempo em que o sistema se estabiliza no prolongamento dessa reta.
-% Para determinar os parâmetros usamos as equações presentes no slide
+hold off;
 
 %% Exercicio 2 (a)
 % Calcular o Ganho K = 5 (Olhando no gráfico)
@@ -158,6 +117,7 @@ step(Hs*5, 'r');
 grid on
 title('Método Phillips e Parr');
 legend({'Dados iniciais', 'Método Phillips e Parr'})
+hold off;
 
 %% ------ Método de Phillips e Parr ------
 dados = load('dadosordem2sub.txt'); 
@@ -174,13 +134,15 @@ wn = 2*pi/T;
 zeta = 0.6/N;
 Teta = 3; 
 G1 = tf(kn*wn^2, [1 2*zeta*wn wn^2], 'InputDelay', Teta);
+hold on;
 plot(t,yn); grid on
 plot(t,yn); grid on;
-hold on;
+
 step(G1, 'r');
 grid on
 title('Método Phillips e Parr');
 legend({'Dados iniciais', 'Método Phillips e Parr'})
+hold off;
 
 %% Exercicio 2 (b)
 N = 2; %Número de ciclos visíveis ocorrem em aproximadamente 10 seg.
@@ -218,6 +180,7 @@ grid on;
 hold on;
 plot (tempo, yn);
 grid on;
+hold off;
 
 %% Exercicio 2 (c)
 % Desconsiderar o tempo morto e condição inicial nula
@@ -247,11 +210,10 @@ Zeta = (0.0805 - 5.547*(0.475 - x)^2)/(x - 0.356); % Fator de Amortecimento
 if Zeta < 1
     F1 = 0.708*(2.811)^Zeta;
     
-else if Zeta >= 1
+else
     F1 = 2.6*Zeta - 0.6;
     Tau_1 = (Zeta + sqrt(Zeta^2 - 1))/Wn;
     Tau_2 = (Zeta - sqrt(Zeta^2 - 1))/Wn;
-    end
 end
 
 Wn = F1/(T3 - T1); % Frequência Natural
@@ -263,6 +225,7 @@ Hs = (exp(Teta*s)*Wn^2)/(s^2 + 2*Zeta*Wn*s + Wn^2);
 hold on;
 step(Hs*5, 'r');
 grid on
+hold off;
 
 %% ------ Método de Mollenkamp ------
 dados = load('dadosordem2sub.txt'); 
@@ -295,6 +258,7 @@ hold on;
 step(G1, 'r');
 grid on
 hold on;
+hold off;
 
 %% Exercicio 2 (d)
 % H(s) = e^(-Td*s)/(T1*s + 1)*(T2*s + 1)
@@ -313,6 +277,7 @@ Hs = exp(-Td*s)/((T1*s + 1)*(T2*s + 1));
 hold on;
 step(Hs, 'r');
 grid on
+hold off;
 
 %% ------ Método de Sundaresan ------
 dados = load('dadosordem2sub.txt'); 
@@ -344,6 +309,7 @@ hold on;
 step(G1, 'r');
 grid on
 hold on;
+hold off;
 
 %% Exercicio 3
 dados = load('dadosordem2aleatorio.txt');
@@ -378,3 +344,4 @@ title('Resposta em Frequência');
 xlabel('Frequência (Hz)');
 ylabel('Amplitude');
 grid on;
+hold off;
